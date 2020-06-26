9ce59db4d5577712ff0f7004a6623fa5a3ab1530
kmucsyj <kmucsyj@kookmin.ac.kr>
2020-06-26 20:57:15 +0900

    사용자가 입력한 값이 없을경우 값을 입력하라고 출력 (amend 가 안된다...) #2

--- calc/SumProduct.py
+++ calc/SumProduct.py
@@ -5,8 +5,10 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
+
     if '' in [a, b]:
 	print("값을 입력하세요")
+
     if '' not in [a, b]:
         a, b = int(a), int(b)
         x = a+b
